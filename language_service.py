5ce4e963747e788a6c923cee6cb1713bd245d8e3
blerdline <blerdline@gmail.com>
2023-12-31 17:49:08 -0600

    Added service for language

--- language_service.py
+++ language_service.py
@@ -1,9 +1,24 @@
+from enum import Enum
+
+class Language(Enum):
+    ENGLISH = "English",
+    SPANISH = "Español"
+
 class LanguageService:
     def __init__(self, database):
         self.database = database
 
     def update_language(self, customer_id, language):
+        if not self.verify_language(language):
+            return f'{language} is not a valid language'
+        
         language_data = {"langPref": language}
         cust_ref = self.database.db.collection('users').document(customer_id)
         cust_ref.set(language_data, merge=True)
         return f'Language updated to {language}'
+    
+    def verify_language(self, language: str):
+        if language in Language.__members__:
+            return True
+        else:
+            return False
